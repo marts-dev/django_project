eef58fb40cd7a96ced425c5dbad739766086f454
marts-dev <8363978+marts-dev@users.noreply.github.com>
2020-08-16 13:29:22 +0800

    Agilelist model creation created migrations and registered model to admin

--- djangobasics/agilelist/models.py
+++ djangobasics/agilelist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Statement(models.Model):
+    STATEMENT_TYPE = [
+        ("value", "Value"),
+        ("principle", "Principle"),
+    ]
+    title: models.CharField = models.CharField(max_length=255)
+    definition: models.TextField = models.TextField(max_length=1000)
+    category: models.CharField = models.CharField(choices=STATEMENT_TYPE, max_length=9)
+
+    def __str__(self):
+        return self.title
